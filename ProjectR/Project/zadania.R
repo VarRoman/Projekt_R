e406b195160b6ef860dd057386e853db20f2b040
yawwkee <117117129+yawwkee@users.noreply.github.com>
2025-05-07 20:33:21 +0200

    Update zadania.R

--- ProjectR/Project/zadania.R
+++ ProjectR/Project/zadania.R
@@ -2,50 +2,30 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 #zadanie 2
-# Wczytaj dane (dostosuj ścieżkę pliku, jeśli trzeba)
+
 df1 <- read_delim("C:\\Users\\mysyu\\Desktop\\HR Analytics\\HR Analytics\\train.csv", delim = ";")
-# Zliczenie pracowników w każdym departamencie
+
 pracownicy_departament <- df1 %>%
   count(department, name = "liczba_pracowników") %>%
   arrange(department)
 
-# Zliczenie pracowników w każdym regionie
 pracownicy_region <- df1 %>%
   count(region, name = "liczba_pracowników") %>%
   arrange(region)
 
-# Wyświetlenie wyników
 print(pracownicy_departament)
 print(pracownicy_region)
-#
-
 #zadanie3
-
-# Tabela liczności awansu wg płci
 tabela_awansow <- table(df1$gender, df1$is_promoted)
-
-# Wyświetlenie tabeli z czytelnymi nazwami
 colnames(tabela_awansow) <- c("Nie awansowany", "Awansowany")
 rownames(tabela_awansow) <- c("Kobiety", "Mężczyźni")
-
-# Wynik
 print(tabela_awansow)
-#
-
 #zadanie4
-
-# Tabela liczności awansów wg kanału rekrutacji
 tabela_kanal_awansu <- table(df1$recruitment_channel, df1$is_promoted)
-
-# Ustawienie czytelnych nazw kolumn
 colnames(tabela_kanal_awansu) <- c("Nie awansowany", "Awansowany")
-
-# Wyświetlenie tabeli
 print(tabela_kanal_awansu)
-#
 
 #zadanie5
-# Wykres 1: no_of_trainings vs gender
 ggplot(df1, aes(x = gender, y = no_of_trainings, fill = gender)) +
   geom_boxplot() +
   labs(
@@ -56,7 +36,6 @@ ggplot(df1, aes(x = gender, y = no_of_trainings, fill = gender)) +
   ) +
   theme_minimal()
 
-# Wykres 2: length_of_service vs gender
 ggplot(df1, aes(x = gender, y = length_of_service, fill = gender)) +
   geom_boxplot() +
   labs(
@@ -66,9 +45,6 @@ ggplot(df1, aes(x = gender, y = length_of_service, fill = gender)) +
     fill = "Płeć"
   ) +
   theme_minimal()
-#
-
-#zadanie5
 ggplot(df1, aes(x = education, y = avg_training_score, fill = education)) +
   geom_boxplot() +
   labs(
@@ -81,8 +57,6 @@ ggplot(df1, aes(x = education, y = avg_training_score, fill = education)) +
   theme(
     axis.text.x = element_text(angle = 45, hjust = 1)  # obrócenie etykiet x dla czytelności
   )
-#
-
 #zadanie6
 ggplot(df1, aes(x = previous_year_rating, y = avg_training_score, color = as.factor(is_promoted))) +
   geom_point(alpha = 0.6) +
@@ -109,7 +83,6 @@ ggplot(df1, aes(x = no_of_trainings, y = age, color = gender)) +
 
 #zadanie8
 
-# Funkcja do obliczania przedziałów ufności
 conf_int_stats <- function(x) {
   n <- length(x)
   m <- mean(x, na.rm = TRUE)
@@ -125,7 +98,7 @@ conf_int_stats <- function(x) {
   list(mean_ci = ci_mean, var_ci = ci_var)
 }
 
-# Użycie funkcji z poprawką
+
 ci_results <- df1 %>%
   filter(!is.na(gender)) %>%
   group_by(gender) %>%
